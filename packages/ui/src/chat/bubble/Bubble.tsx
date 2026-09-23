a989beecd3ca3d05d270daa9737269e45279c343
iHeyTang <dehui1012@qq.com>
2026-09-23 16:39:16 +0800

    fix(ui): retain a small gap beneath the message tab

--- packages/ui/src/chat/bubble/Bubble.tsx
+++ packages/ui/src/chat/bubble/Bubble.tsx
@@ -1948,7 +1948,7 @@ function UserStickyBubbleUnmemoized({
   return (
     <div className="sticky top-0 z-20">
       <TooltipProvider delayDuration={180} skipDelayDuration={80}>
-        <div data-user-message-chrome className="group">
+        <div data-user-message-chrome className="group pb-1">
           <div className="relative">
             <div
               data-background-surface="sticky-message"
